36f25b8c10573cb77abd0665796ab61c12daf148
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-01-22 17:29:17 +0100

    Create basic Gateway page

---
+++ packages/web/docs/src/app/gateway/page.tsx
@@ -0,0 +1,48 @@
+import { Metadata } from 'next';
+import { CallToAction, ExploreMainProductCards, Hero, HiveGatewayIcon } from '@theguild/components';
+import { LandingPageContainer } from '../../components/landing-page-container';
+import { metadata as rootMetadata } from '../layout';
+
+export const metadata: Metadata = {
+  title: 'Hive Gateway',
+  // TODO:
+  // description:
+  //   'Fully Open-Source schema registry, analytics and gateway for GraphQL federation and other GraphQL APIs',
+  alternates: {
+    // to remove leading slash
+    canonical: '.',
+  },
+  openGraph: {
+    ...rootMetadata.openGraph,
+    // to remove leading slash
+    url: '.',
+  },
+};
+
+export default function HiveGatewayPage() {
+  return (
+    <LandingPageContainer className="text-green-1000 light mx-auto max-w-[90rem] overflow-hidden px-6">
+      <Hero
+        logo={<HiveGatewayIcon />}
+        heading="Hive Gateway"
+        text="Unify and accelerate your data graph across diverse services with Hive Gateway, which seamlessly integrates with Apollo Federation."
+        checkmarks={['Fully open source', 'No vendor lock-in', 'Can be self-hosted!']}
+      >
+        <CallToAction variant="primary" href="/docs/gateway">
+          Get Started
+        </CallToAction>
+        <CallToAction variant="tertiary" href="https://github.com/graphql-hive/gateway">
+          GitHub
+        </CallToAction>
+        {/* TODO: decoration */}
+      </Hero>
+      {/* observability and performance monitoring */}
+      {/* orchestrate your way */}
+      {/* Federation-Compatible Gateway Benchmarks */}
+      {/* Let's get advanced */}
+      {/* Cloud-Native Nature */}
+      <ExploreMainProductCards />
+      {/* big get your API game right section */}
+    </LandingPageContainer>
+  );
+}
